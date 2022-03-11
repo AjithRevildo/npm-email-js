6254d3d23598e7af302e71f61df341b144199b3d
Ajith Revildo <ajithrevildo1999@gmail.com>
2022-03-12 03:23:49 +0530

    secound commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import emailjs from "emailjs-com";
 
 function App() {
+  function sendEmail(e) {
+    e.preventDefault();
+
+emailjs.sendForm('gmail', 'template_k1imgbo', e.target,'xh_KH5LykLTzz_PqW')
+    .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log(error.text);
+    });
+    e.target.reset()
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+      <form onSubmit={sendEmail}>
+                  <div className="row pt-5 mx-auto">
+                      <div className="col-8 form-group mx-auto">
+                          <input type="text" className="form-control" placeholder="Name" name="name"/>
+                      </div>
+                      <div className="col-8 form-group pt-2 mx-auto">
+                          <input type="email" className="form-control" placeholder="Email Address" name="email"/>
+                      </div>
+                      <div className="col-8 form-group pt-2 mx-auto">
+                          <input type="text" className="form-control" placeholder="Subject" name="subject"/>
+                      </div>
+                      <div className="col-8 form-group pt-2 mx-auto">
+                          <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message"></textarea>
+                      </div>
+                      <div className="col-8 pt-3 mx-auto">
+                          <input type="submit" className="btn btn-info" value="Send Message"></input>
+                      </div>
+                  </div>
+              </form>
+          </div>
+      </div>
   );
 }
 
 export default App;
+
+
+
+
+
